ccb68dbc80298a36a5582e5eb89c04e200dbe827
Michal Kijewicz <michal.kijewicz@nielsen.com>
2020-10-05 15:43:23 +0200

    Test ws

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const httpHandler = async () => {
+    const response = await fetch("http://192.168.1.181:8000/", {
+      method: "get",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log(await response.json());
+  };
+
+  const wsHandler = () => {
+    const socket = new WebSocket("ws://192.168.1.181:8000/ws");
+
+    socket.addEventListener("open", function (event) {
+      socket.send("Hello Server!");
+    });
+
+    socket.addEventListener("message", function (event) {
+      console.log("Message from server ", event.data);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={httpHandler}>HTTP</button>
+        <button onClick={wsHandler}>WS</button>
       </header>
     </div>
   );
