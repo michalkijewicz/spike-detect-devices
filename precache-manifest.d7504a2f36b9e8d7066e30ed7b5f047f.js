80548e3ec8a27fd2703a2f0879bcee5ff8572289
Michal Kijewicz <michal.kijewicz@nielsen.com>
2020-10-05 15:00:38 +0200

    Updates

---
+++ precache-manifest.d7504a2f36b9e8d7066e30ed7b5f047f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4735961f319b0968e51de51b56521a47",
+    "url": "/spike-detect-devices/index.html"
+  },
+  {
+    "revision": "0ee617058fcd8c24234f",
+    "url": "/spike-detect-devices/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6374f1e065f20841d7dd",
+    "url": "/spike-detect-devices/static/js/2.7ba5c3a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spike-detect-devices/static/js/2.7ba5c3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee617058fcd8c24234f",
+    "url": "/spike-detect-devices/static/js/main.28aece46.chunk.js"
+  },
+  {
+    "revision": "9653ecb4af2aec5eeb56",
+    "url": "/spike-detect-devices/static/js/runtime-main.b0b78c0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spike-detect-devices/static/media/logo.5d5d9eef.svg"
+  }
+]);
